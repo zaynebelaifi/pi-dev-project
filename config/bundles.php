5ea01b2fb31c4dd405b9ac66783b61bd7c9ced6f
Mouadh Farhat <fmouadh50@gmail.com>
2026-04-08 17:39:46 +0100

    Integration

--- config/bundles.php
+++ config/bundles.php
@@ -8,7 +8,7 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
-    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['prod' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
