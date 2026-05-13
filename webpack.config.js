f7184ceb9c1fe2d9cf0a27b63d36effb7a313cc7
wissem benali <wissemwork10@gmail.com>
2026-05-13 15:13:35 +0100

    Fix Webpack Encore production build

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const Encore = require('@symfony/webpack-encore');
+
+if (!Encore.isRuntimeEnvironmentConfigured()) {
+    Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'production');
+}
+
+Encore
+    .setOutputPath('public/build/')
+    .setPublicPath('/build')
+    .addEntry('app', './assets/app.js')
+    .enableStimulusBridge('./assets/controllers.json')
+    .splitEntryChunks()
+    .enableSingleRuntimeChunk()
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+    .enableSassLoader()
+    .enablePostCssLoader();
+
+module.exports = Encore.getWebpackConfig();
